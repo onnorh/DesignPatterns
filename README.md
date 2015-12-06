c9dd47dba6ef840c572cd6bbacca45b872571ec6
Chee Zhiquan <zhiquan@chees-imac.gateway.2wire.net>
2015-12-06 23:04:09 +0800

    Added Strategy Pattern - Javascript

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Design Patterns
 Python and Javascript Implementation to the design patterns, using social network as the subject
 
-Currently learning software design patterns through reading books and online resources, I would be writing codes that implements these design patterns using my own interpretation.
+Currently learning software design patterns through reading books and online resources, I will be writing codes that implements these design patterns using my own interpretation.
 
-Although this may serve as a reference for other developers out there, I cannot guarantee that my interpretation is 100% correct. So, please feel free to correct my definition or my code even.
+This is for my own development but I hope this can be useful to other developers as well. Although this may serve as a reference for other developers out there, I cannot guarantee that my interpretation is 100% correct. So, please feel free to correct my definition or my code even.
+
+I will try to make the definition as simple as possible. The examples will fall under the social media category where more people can relate easily.
 
 Thanks for taking the time to review this repository!
---
+++ StrategyPattern.js
@@ -0,0 +1,75 @@
+"use strict";
+/**
+  * Strategy Pattern - Separating a set of algorithms by encapsulating each of them,
+  * and making them interchangeable. This concept is similar to a war game where you
+  * would have different ways of fighting the opponent. You start with the same set
+  * of resources each game, and then ending the game by defeating the opponent. The
+  * end goal remains the same throughout all the games.
+  *
+  * Example - You want to log in to a website, the website offers many ways of logging
+  * on. You may choose to use your Facebook account, Google Plus, LinkedIn. However,
+  * these require different login mechanism. So let's keep the complexity of each
+  * mechanism to its own class.
+  *
+  * Note: This pattern is meant to provide different ways of achieving the goal with
+  * the same starting resources. Therefore, the functions shouldn't contain different
+  * parameters.
+  */
+
+class LoginMethod {
+    login() {
+        console.log("Please choose a login method");
+    }
+}
+
+class FacebookLogin extends LoginMethod {
+    login() {
+        console.log(`Logging with Facebook Account...
+Redirected to Facebook Page to authorise this application
+Successfully logged on`);
+    }
+}
+
+class GoogleLogin extends LoginMethod {
+    login() {
+        console.log(`Logging with Google Account...
+Redirected to Google Page to authorise this application
+Google requires mobile verification code
+Successfully verified
+Successfully logged on`);
+    }
+}
+
+class LinkedInLogin extends LoginMethod {
+    login() {
+        console.log(`Logging with LinkedIn Account...
+ Redirected to LinkedIn to authorise this application
+ Successfully logged on`);
+    }
+}
+
+class User {
+    constructor() {
+        this.loginMethod = new LoginMethod();
+    }
+    setLoginMethod(loginMethod) {
+        this.loginMethod = loginMethod;
+    }
+    performLogin() {
+        this.loginMethod.login();
+    }
+}
+
+// This user has no login method defined
+let user1 = new User();
+user1.performLogin();
+
+// This user uses Facebook
+let user2 = new User();
+user2.setLoginMethod(new FacebookLogin());
+user2.performLogin();
+
+// This user uses Google
+let user3 = new User();
+user3.setLoginMethod(new GoogleLogin());
+user3.performLogin();
