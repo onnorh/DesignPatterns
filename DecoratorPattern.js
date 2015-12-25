5a4c0526d2697d0a53467a2a115face4b357b45b
Chee Zhiquan <zhiquan.chee@skyscanner.net>
2015-12-25 20:47:08 +0800

    Updated DecoratorPattern.js

--- DecoratorPattern.js
+++ DecoratorPattern.js
@@ -10,5 +10,48 @@
   * to equip the membership with more addons but it comes with a cost. The membership
   * fee may be increased. Decorator Pattern can be used in this scenario
   *
-  * In ES2016, there is a decorator syntax in Javascript
   */
+
+class Membership {
+    constructor(addons, cost) {
+        this.addons = addons;
+        this.cost = cost;
+    }
+    getAddons() {
+        return this.addons;
+    }
+    getCost() {
+        return this.cost;
+    }
+}
+
+let addOnPost = (membership) => {
+    let _getAddons = membership.getAddons();
+    membership.getAddons = () => {
+        let addons = _getAddons;
+        addons.push("CanPost")
+        return addons;
+    }
+    let _getCost = membership.getCost();
+    membership.getCost = () => {
+        return _getCost + 0.99;
+    }
+}
+
+let addOnAdmin = (membership) => {
+    let _getAddons = membership.getAddons();
+    membership.getAddons = () => {
+        let addons = _getAddons;
+        addons.push("IsAdmin");
+        return addons;
+    }
+}
+
+
+let jack001 = new Membership(["CanView"], 0.8);
+console.log(jack001.getCost());
+addOnPost(jack001);
+console.log(jack001.getCost());
+addOnAdmin(jack001);
+console.log(jack001.getCost());
+console.log(jack001.getAddons());
