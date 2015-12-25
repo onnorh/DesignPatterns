6ffa7e38726d31dfb893bc38437fcb3a686518af
Chee Zhiquan <zhiquan.chee@skyscanner.net>
2015-12-25 21:18:00 +0800

    Added empty DecoratorPattern.py

---
+++ DecoratorPattern.py
@@ -0,0 +1,13 @@
+"""
+   Decorator Pattern - Adding extra functionalities to an object without affecting
+   its structure. Like the name suggests, this pattern is used to add additional
+   features to an item. Similar to decorating a christmas tree, topping up with
+   christmas lights and other ornaments to look better.
+
+   Example - Your social network account might need certain membership addons to
+   to get access to more information or functions. Purchasing the addons is one way
+   to equip the membership with more addons but it comes with a cost. The membership
+   fee may be increased. Decorator Pattern can be used in this scenario.
+
+   Note: Python has decorator syntax
+"""
