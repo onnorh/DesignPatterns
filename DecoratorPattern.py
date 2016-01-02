3931030f9926646dfb9d9eee5d1e7f4bf9b2e73b
Chee Zhiquan <zhiquan@chees-imac.gateway.2wire.net>
2016-01-02 17:07:15 +0800

    Updated DecoratorPattern.py

--- DecoratorPattern.py
+++ DecoratorPattern.py
@@ -9,5 +9,41 @@
    to equip the membership with more addons but it comes with a cost. The membership
    fee may be increased. Decorator Pattern can be used in this scenario.
 
-   Note: Python has decorator syntax
 """
+
+class Membership(object):
+    def __init__(self, addons, cost):
+        self.addons = addons
+        self.cost = cost
+
+    def getAddons(self):
+        return self.addons
+
+    def getCost(self):
+        return self.cost
+
+def addOnPost(func):
+    def func_wrapper(self=None):
+        _addOns = func(self)
+        _addOns.append("CanPost")
+        return _addOns
+
+    return func_wrapper
+
+def addCostPost(func):
+    def func_wrapper(self=None):
+        return func(self) + 0.8
+    return func_wrapper
+
+class BronzeMembership(Membership):
+    @addOnPost
+    def getAddons(self):
+        return self.addons
+
+    @addCostPost
+    def getCost(self):
+        return self.cost
+
+bronzeMember = BronzeMembership(["CanView"], 0.8)
+print bronzeMember.getAddons()
+print bronzeMember.getCost()
