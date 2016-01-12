2c02254c1f917101b8e452c80a98f86b7f029e52
Chee Zhiquan <zhiquan.chee@skyscanner.net>
2016-01-12 22:17:28 +0800

    Implemented Abstract Factory Pattern Javascript

---
+++ AbstractFactoryPattern.js
@@ -0,0 +1,54 @@
+"use strict";
+/**
+  * Abstract Factory Pattern - provides an interface for creating families of
+  * related or dependent objects without specifying their concrete classes. It
+  * acts like a salesman who creates a generic order form, find clients, take
+  * orders and send the orders to a group of factories to make the products.
+  *
+  * Example - Different types of user may opt for a set of properties in their
+  * account, these set of properties are also differentiated by the type of the user.
+  * Say, an American User uses a different type of profile layout and payment
+  * service as compared to a British User, this difference can be due to culture
+  * or environment
+  *
+  */
+
+class UserFactory {
+
+}
+
+class AmericanUserFactory {
+
+}
+
+class BritishUserFactory {
+
+}
+
+class PropertiesFactory{
+
+}
+
+class AmericanUserPropertiesFactory {
+
+}
+
+class BritishUserPropertiesFactory {
+
+}
+
+class AmericanUserProfileLayout {
+
+}
+
+class BritishUserProfileLayout {
+
+}
+
+class AmericanUserPaymentService {
+
+}
+
+class BritishUserPaymentService {
+    
+}
